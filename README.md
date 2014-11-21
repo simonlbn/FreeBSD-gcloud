b3d9d9ebfa6535da48c7636e0acc8e32852eb615
Steve Wills <steve@mouf.net>
2014-11-21 04:03:46 +0000

    improvements

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Script to generate FreeBSD Google Cloud images
 
 Here's the basic beginnings of my script to create Google Cloud images for
 FreeBSD. Needs lots of work.
+
+* Requires these pkgs (ports):
+    bar (textproc/bar)
+    gtar (archivers/gtar)
+    google-cloud-sdk (net/google-cloud-sdk)
+
+* best run on the same version of the OS that images are being created for,
+  since newfs is used from the host
+
+* You should login to GCloud before running
--- build_google_image.sh
+++ build_google_image.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-set -e
-set -x
+# Script to create images for use in Google Cloud Compute
 
-BASEDIR=$(dirname $0)
-WRKDIR=${PWD}
+# TODO: configure to use google NTP servers - metadata.google.internal (169.254.169.254)
+# TODO: configure syslog to use serial?
 
-TS=`env TZ=UTC date +%Y%m%d%H%M%S`
+###############################
+# tweak these to taste
 
 # release to use
 VERSION=10.1-RELEASE
-
 # see truncate(1) for acceptable sizes
 VMSIZE=10g
+# size passed to mkimg(1)
 SWAPSIZE=1G
+
+# which bucket to upload to
+MYBUCKET=swills-test-bucket
+TS=`env TZ=UTC date +%Y%m%d%H%M%S`
+IMAGENAME=FreeBSD-${VERSION}-amd64-${TS}
+BUCKETFILE=${IMAGENAME}.tar.gz
 
 TMPFILE=FreeBSD-${VERSION}-amd64-gcloud-image-${TS}.raw
 
 ###############################
+
+BASEDIR=$(dirname $0)
+WRKDIR=${PWD}
 
 # fetch base and kernel for this version
 mkdir -p ${VERSION}
@@ -41,8 +50,8 @@ mkdir -p /mnt/g/new
 mount /dev/${MD_UNIT} /mnt/g/new
 
 cd /mnt/g/new
-tar -xzvf ${WRKDIR}/${VERSION}/base.txz
-tar -xzvf ${WRKDIR}/${VERSION}/kernel.txz
+bar -n ${WRKDIR}/${VERSION}/base.txz   | tar -xzf -
+bar -n ${WRKDIR}/${VERSION}/kernel.txz | tar -xzf -
 
 cp /etc/resolv.conf etc/resolv.conf
 
@@ -56,11 +65,15 @@ nameserver 169.254.169.254
 nameserver 8.8.8.8
 EOF
 
+cat etc/resolv.conf
+
 cat << EOF > etc/fstab
 # Custom /etc/fstab for FreeBSD VM images
 /dev/gpt/rootfs   /       ufs     rw      1       1
-/dev/gpt/swapfs  none    swap    sw      0       0
+/dev/gpt/swapfs   none    swap    sw      0       0
 EOF
+
+cat etc/fstab
 
 cat << EOF > etc/rc.conf
 console="comconsole"
@@ -72,18 +85,21 @@ sshd_enable="YES"
 google_accounts_manager_enable="YES"
 EOF
 
+cat etc/rc.conf
+
 cat << EOF > boot/loader.conf
 console="comconsole"
 hw.memtest.tests="0"
 kern.timecounter.hardware=ACPI-safe
 EOF
 
+cat boot/loader.conf
+
 cat << EOF >> etc/hosts
 169.254.169.254 metadata.google.internal metadata
 EOF
 
-# TODO: configure to use google NTP servers - metadata.google.internal (169.254.169.254)
-# TODO: configure syslog to use serial?
+cat etc/hosts
 
 sync
 sync
@@ -108,6 +124,8 @@ mkimg -s gpt -b pmbr \
         -o disk.raw
 sync
 
-# gtar -Szcf FreeBSD-${VERSION}-amd64-${TS}.tar.gz disk.raw
-# gsutil cp FreeBSD-10.1-RELEASE-amd64-20141118052210.1.tar.gz gs://swills-test-bucket
-# gcutil addimage freebsd-101 gs://swills-test-bucket/FreeBSD-10.1-RELEASE-amd64-20141118052210.1.tar.gz
+gtar -Szcf ${BUCKETFILE} disk.raw
+rm ${TMPFILE} disk.raw pmbr gptboot
+
+gsutil cp FreeBSD-${VERSION}-amd64-${TS}.tar.gz gs://${MYBUCKET}
+gcutil addimage ${IMAGENAME} gs://${MYBUCKET}/${BUCKETFILE}
